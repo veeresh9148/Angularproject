ecc43e3cb366651f86d40b6332fda174a8d6e489
veeresh9148 <102301570+veeresh9148@users.noreply.github.com>
2022-12-02 17:28:24 +0530

    Modified by veeresh
    Modified by veeresh

---
+++ Angular-Assignment-2/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OverlayComponent } from './overlay/overlay.component';
+import { AddItemComponent } from './add-item/add-item.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { CartListComponent } from './cart-list/cart-list.component';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OverlayComponent,
+    AddItemComponent,
+    ItemListComponent,
+    CartListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
+ 
